3e79e61493c090c683b341cb85d426f300d9c15d
Abhishek Verma <www.abhiverma23@gmail.com>
2022-10-07 18:48:37 +0530

    Run entire app in strict mode

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import React from 'react';
 function App() {
     return (
     <div>
-        <React.StrictMode>
-            <h1>Hello world</h1>
-        </React.StrictMode>
+        <h1>Hello world</h1>
     </div>
     );
 }
